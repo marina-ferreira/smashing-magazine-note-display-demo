92259cfbddd3654d0185d922e1fe7fb958dcc974
Marina Ferreira <marina.ferreira.dev@gmail.com>
2018-12-18 06:41:12 -0200

    Fill animation to note value

--- scripts.js
+++ scripts.js
@@ -2,12 +2,19 @@ const displays = document.querySelectorAll('.note-display');
 const transitionDuration = 900;
 
 displays.forEach(display => {
+  let note = parseFloat(display.dataset.note);
+
+  strokeTransition(display, note);
+});
+
+function strokeTransition(display, note) {
   let progress = display.querySelector('.circle__progress--fill');
   let radius = progress.r.baseVal.value;
   let circumference = 2 * Math.PI * radius;
+  let offset = circumference * (10 - note) / 10;
 
-  progress.style.setProperty('--transitionDuration', `${transitionDuration}ms`);
   progress.style.setProperty('--initialStroke', circumference);
+  progress.style.setProperty('--transitionDuration', `${transitionDuration}ms`);
 
-  setTimeout(() => progress.style.strokeDashoffset = 50, 100);
-});
+  setTimeout(() => progress.style.strokeDashoffset = offset, 100);
+}
